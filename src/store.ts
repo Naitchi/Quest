66d218cff1b4f1fb55eab53cebb3caf960d3cf98
Naitchi <assasindu39@gmail.com>
2023-04-24 17:28:58 +0200

    🚀 First Commit

---
+++ src/store.ts
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state: {
+    // votre état initial
+  },
+  mutations: {
+    // vos mutations
+  },
+  actions: {
+    // vos actions
+  },
+  getters: {
+    // vos getters
+  },
+});
+
+export default store;
