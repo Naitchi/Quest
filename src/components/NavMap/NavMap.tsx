3c7f9871f2ee1f7aebae2de98742c49dfdb147c2
Naitchi <assasindu39@gmail.com>
2023-12-06 17:02:37 +0100

    Upgraded TarkovMap to be more like the game

--- src/components/NavMap/NavMap.tsx
+++ src/components/NavMap/NavMap.tsx
@@ -8,14 +8,33 @@ import styles from './NavMap.module.css';
 export default function NavMap() {
   return (
     <div className={styles.map}>
-      <Link className={styles.customs} href="/customs"></Link>
-      <Link className={styles.factory} href="/factory"></Link>
-      <Link className={styles.woods} href="/woods"></Link>
-      <Link className={styles.reserve} href="/reserve"></Link>
-      <Link className={styles.lighthouse} href="/lighthouse"></Link>
-      <Link className={styles.shoreline} href="/shoreline"></Link>
-      <Link className={styles.interchange} href="/interchange"></Link>
-      <Link className={styles.streets} href="/streets"></Link>
+      <Link className={`${styles.lien} ${styles.customs}`} href="/customs">
+        CUSTOMS
+      </Link>
+      <Link className={`${styles.lien} ${styles.factory}`} href="/factory">
+        FACTORY
+      </Link>
+      <Link className={`${styles.lien} ${styles.woods}`} href="/woods">
+        WOODS
+      </Link>
+      <Link className={`${styles.lien} ${styles.reserve}`} href="/reserve">
+        RESERVE
+      </Link>
+      <Link className={`${styles.lien} ${styles.lighthouse}`} href="/lighthouse">
+        LIGHTHOUSE
+      </Link>
+      <Link className={`${styles.lien} ${styles.shoreline}`} href="/shoreline">
+        SHORELINE
+      </Link>
+      <Link className={`${styles.lien} ${styles.interchange}`} href="/interchange">
+        INTERCHANGE
+      </Link>
+      <Link className={`${styles.lien} ${styles.streets}`} href="/streets">
+        STREETS OF <span className={styles.tarkov}>TARKOV</span>
+      </Link>
+      <Link className={`${styles.lien} ${styles.lab}`} href="/lab">
+        THE LAB
+      </Link>
     </div>
   );
 }
