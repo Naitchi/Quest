f0a06460e64014e503823dd1e93c3d9125e7c72f
Naitchi <assasindu39@gmail.com>
2024-01-11 13:45:58 +0100

    🐛 Fix hover description to multiplayer button

--- src/components/MultiplayerBtn/MultiplayerBtn.tsx
+++ src/components/MultiplayerBtn/MultiplayerBtn.tsx
@@ -55,14 +55,14 @@ export default function MultiplayerBtn() {
         <div
           style={{
             position: 'absolute',
-            top: '50%',
-            left: '100%',
+            top: '5%',
+            left: '5%',
             minWidth: '250px',
             backgroundColor: 'rgba(0, 0, 0, 0.8)',
             color: '#fff',
             padding: '7px',
             borderRadius: '5px',
-            zIndex: 1,
+            zIndex: 2,
           }}
         >
           Lift the level and faction restrictions to add your friends&apos; quests.
